6ec9ac62eb226d6022db55aed530290f2879bb5c
DrazzDan <81708903+DrazzDan@users.noreply.github.com>
2021-05-06 16:04:23 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -9,7 +9,7 @@ from config import BOT_NAME as bn
 @Client.on_message(filters.command("start") & filters.private & ~filters.channel)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""Hello [👋](https://telegra.ph/file/e9aeacff57c03e6b3ac90.jpg) there! I'm Tohka! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n🔴 Do you want me to play music in your Telegram groups'voice chats? Please click the \'📜 User Manual 📜\' button below to know how you can use me.\n\n🔴 The Assistant must be in your group to play music in the voice chat of your group.""",
+        f"""Hello 👋 there! I'm Tohka! I can play music in voice chats of Telegeam Groups. I have a lot of cool feature that will amaze you!\n\n🔴 Do you want me to play music in your Telegram groups'voice chats? Please click the \'📜 User Manual 📜\' button below to know how you can use me.\n\n🔴 The Assistant must be in your group to play music in the voice chat of your group.""",
         reply_markup=InlineKeyboardMarkup(
             [ 
                 [
@@ -21,7 +21,9 @@ async def start(_, message: Message):
                     ), 
                     InlineKeyboardButton(
                         "Support", url="https://t.me/WeebXWorld") 
-                ]
+                ],[
+                  InlineKeyboardButton(
+                        "Reports bugs and queries", url="https://t.me/Tohka_reports_bot") 
             ]
         ),
      disable_web_page_preview=True
@@ -34,7 +36,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "📜 User Manual 📜", url="https://telegra.ph/Tohka-Yatogami-04-20")
+                        "📜 User Manual 📜", url="https://t.me/Tohkanews/4")
                 ]
             ]
         )
